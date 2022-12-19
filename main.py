dda53caf6f8b7b68d3ca2124807cf6305c6585c5
Andreas Himmel <andreas.himmel@iat.tu-darmstadt.de>
2022-12-19 17:48:40 +0100

    some code to check Tensor

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import multilinear_algebra as ma
 
 if __name__ == "__main__":
-    n_dim = 2
-    s = ma.MLA.scalar(2)
-    u = ma.MLA.parameter("u")
-    v = ma.MLA.parameter("v")
+    tensor1 = ma.Tensor(type="^_", dimension=2, name="A")
 
-    A = ma.MLA(tensor_type=["^_"], name="A", dim=n_dim)
-    A.get_random_values()
-    print("the components of A are given by: ", A)
-    A.print_components()
+    tensor2 = ma.Tensor()
+    tensor2.initialize_tensor({"type": "__", "dimension": [2, 2], "name": "B"})
+
+    tensor3 = ma.Tensor()
+    tensor3.initialize_tensor(
+        {
+            "type": "__",
+            "dimension": [2, 2],
+            "name": "C",
+            "values": {(0, 0): 1, (0, 1): 1, (1, 0): 2, (1, 1): 3},
+        }
+    )
+
+    tensor4 = ma.Tensor()
+    tensor4.initialize_tensor(
+        {
+            "type": "__",
+            "dimension": [2, 2],
+            "name": "D",
+        }
+    )
+    tensor4.assign_values(values={(0, 0): 3, (0, 1): -3.5, (1, 0): 2.14, (1, 1): -0.3})
+    print(tensor4)
+
+    print(ma.MLA.scalar(2))
