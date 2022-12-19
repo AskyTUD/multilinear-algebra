7e7358d27df5f0a2872e0d05a756e38c8546c84e
Andreas Himmel <andreas.himmel@iat.tu-darmstadt.de>
2022-12-19 20:09:13 +0100

    initial stage for test tensor

---
+++ test/test_tensor.py
@@ -0,0 +1,18 @@
+import unittest
+
+import casadi as ca
+import numpy as np
+
+import multilinear_algebra as ma
+
+
+class TestOperation(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def test_initTensor(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
