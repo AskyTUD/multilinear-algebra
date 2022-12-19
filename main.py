e7a0b1176f10d032e38ce2bf1baedea5f88ac41a
Andreas Himmel <andreas.himmel@iat.tu-darmstadt.de>
2022-12-19 12:09:50 +0100

    generated new class

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import multilinear_algebra as ma
 
 if __name__ == "__main__":
-    a = ma.MLA.scalar(2)
-    print("the name of the scalar is: " + str(a))
+    n_dim = 2
+    s = ma.MLA.scalar(2)
+    u = ma.MLA.parameter("u")
+    v = ma.MLA.parameter("v")
+
+    A = ma.MLA(tensor_type=["^_"], name="A", dim=n_dim)
+    A.get_random_values()
+    print("the components of A are given by: ", A)
+    A.print_components()
---
+++ multilinear_algebra/tensor.py
@@ -0,0 +1,33 @@
+#
+#   This file is part of multilinear_algebra
+#
+#   multilinear_algebra is a package providing
+#   methods for addition, multiplication, representation, etc., of multilinear objects
+#
+#   Copyright (c) 2022 Andreas Himmel
+#                      All rights reserved
+#
+#   multilinear_algebra is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Lesser General Public License as
+#   published by the Free Software Foundation, either version 3
+#   of the License, or (at your option) any later version.
+#
+#   multilinear_algebra is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#   GNU Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public License
+#   along with multilinear_algebra. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import itertools as it
+import random as rd
+import typing
+import warnings
+
+import numpy as np
+from casadi import casadi as ca
+from tabulate import tabulate
+
+import multilinear_algebra.efun as ef
