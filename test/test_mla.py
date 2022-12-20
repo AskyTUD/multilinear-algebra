8ce838f5c50baf776886d49deef79f86ef5b92af
Andreas Himmel <andreas.himmel@iat.tu-darmstadt.de>
2022-12-20 14:07:41 +0100

    comment os and sys

--- test/test_mla.py
+++ test/test_mla.py
@@ -1,9 +1,9 @@
-import os
-import sys
+# import os
+# import sys
 
-currentdir = os.path.dirname(os.path.realpath(__file__))
-parentdir = os.path.dirname(currentdir)
-sys.path.append(parentdir)
+# currentdir = os.path.dirname(os.path.realpath(__file__))
+# parentdir = os.path.dirname(currentdir)
+# sys.path.append(parentdir) 
 
 import unittest
 
